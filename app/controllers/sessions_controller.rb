a4c292e69f3fa72380aae01aa7a56c3a4cea2d6b
maikikukawa <mai-kikukawa@dac.co.jp>
2016-06-16 09:25:31 +0000

    session_control path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:info] = "logged in as #{@user.name}"
-      redirect_to "/messages/index"
+      redirect_to root_path
     else
       flash[:danger] = 'invalid email/password combination'
       render 'new'
