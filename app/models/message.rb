06a1712485f5a8612c3a71ad707c92be41892aba
maikikukawa <mai-kikukawa@dac.co.jp>
2016-06-23 07:05:49 +0000

    karamu henkou

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,15 +12,23 @@ class Message < ActiveRecord::Base
     CSV.generate do |csv|
       # column_namesはカラム名を配列で返す
       # 例: ["id", "name", "price", "released_on", ...]
-      csv << column_names
+      csv << csv_column_names
       all.each do |message|
         # attributes はカラム名と値のハッシュを返す
         # 例: {"id"=>1, "name"=>"レコーダー", "price"=>3000, ... }
         # valudes_at はハッシュから引数で指定したキーに対応する値を取り出し、配列にして返す
         # 下の行は最終的に column_namesで指定したvalue値の配列を返す
-        csv << message.attributes.values_at(*column_names)
+        csv << message.csv_column_values
       end
     end
+  end
+  
+  def self.csv_column_names
+    ["ID", "広告種別", "媒体種別", "出稿開始日", "出稿終了日","リンク先URL","生成URL","発行ID"]
+  end
+
+  def csv_column_values
+    [id, tipe, media, start, finish, rink, createdurl, createdid]
   end
   
 end
