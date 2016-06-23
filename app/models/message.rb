4bd1674afc6da9abbc4f0ba7177ed5cecaa382b4
maikikukawa <mai-kikukawa@dac.co.jp>
2016-06-23 09:55:31 +0000

    exel update

--- app/models/message.rb
+++ app/models/message.rb
@@ -41,13 +41,29 @@ class Message < ActiveRecord::Base
   end
   
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+
+    (2..spreadsheet.last_row).each do |i|
+      # {カラム名 => 値, ...} のハッシュを作成する
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
       message = find_by(id: row["id"]) || new
       # CSVからデータを取得し、設定する
       message.attributes = row.to_hash.slice(*updatable_attributes)
       # 保存する
       message.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when '.csv'  then Roo::Csv.new(file.path,    nil, :ignore)
+    when '.xls'  then Roo::Excel.new(file.path,  nil, :ignore)
+    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+    when '.ods'  then Roo::OpenOffice.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
     end
   end
 
